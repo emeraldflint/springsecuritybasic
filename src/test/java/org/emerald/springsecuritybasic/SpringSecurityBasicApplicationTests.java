5793a359071266fed3841a78b99b52f5a825f6ec
Eugene <eugene.kasap@gmail.com>
2024-04-01 20:33:54 +0200

    Add spring-boot-starter-security dependency

--- src/test/java/org/emerald/springsecuritybasic/SpringSecurityBasicApplicationTests.java
+++ src/test/java/org/emerald/springsecuritybasic/SpringSecurityBasicApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringsecuritybasicApplicationTests {
+class SpringSecurityBasicApplicationTests {
 
     @Test
     void contextLoads() {
