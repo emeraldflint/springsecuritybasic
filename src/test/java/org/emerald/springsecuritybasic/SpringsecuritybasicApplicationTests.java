d90755032b3927d9293eb9db4b34f2bf742a48f4
Eugene <eugene.kasap@gmail.com>
2024-04-01 18:02:12 +0200

    Initial commit: Add project springsecuritybasic and .gitignore

---
+++ src/test/java/org/emerald/springsecuritybasic/SpringsecuritybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emerald.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
